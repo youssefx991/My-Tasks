8c4d128e2e834d872af6cacfd7463c16f98d2270
Youssef Wagih <youssefwagih212@gmail.com>
2026-04-05 01:53:04 +0200

    add responsive day 1

---
+++ Responsive/Day1/Task/Day1.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('btn_task1').addEventListener('click', ()=>{
+        window.location.href = 'Task1.html'
+    })    
+
+    document.getElementById('btn_task2').addEventListener('click', ()=>{
+        window.location.href = 'Task2.html'
+    })    
+    document.getElementById('btn_task3').addEventListener('click', ()=>{
+        window.location.href = 'Task3.html'
+    })    
+})
+
+
+/*
+<input type="button" value="Task 1" id="btn_task1">
+<input type="button" value="Task 2" id="btn_task2">
+<input type="button" value="Task 3" id="btn_task3">
+*/
