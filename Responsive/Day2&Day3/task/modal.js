b2c92133e9adf05c031ef384dcade8974d544f67
Youssef Wagih <youssefwagih212@gmail.com>
2026-04-08 02:05:26 +0200

    add task 2&3 responsive solution

--- Responsive/Day2&Day3/task/modal.js
+++ Responsive/Day2&Day3/task/modal.js
@@ -3,10 +3,12 @@ const form = document.getElementById('form');
 form.addEventListener('input', () => {
     if (form.checkValidity()) {
         document.getElementById('h1_form_submit').textContent = '';
+        document.getElementById('h1_form_submit').classList.remove('text-danger');
     }
     else 
     {
         document.getElementById('h1_form_submit').textContent = 'Please fill out the form correctly.';
+        document.getElementById('h1_form_submit').classList.add('text-danger');
     }
 });
 
@@ -17,6 +19,8 @@ form.addEventListener('submit', (e) => {
     if (form.checkValidity()) {
         form.reset();
         document.getElementById('h1_form_submit').textContent = 'Form submitted successfully!';
+        document.getElementById('h1_form_submit').classList.remove('text-danger');
+        document.getElementById('h1_form_submit').classList.add('text-success');
     }
 
     
