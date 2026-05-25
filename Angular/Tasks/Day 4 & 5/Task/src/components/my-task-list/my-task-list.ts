92746d3089214bd531a21b77486eb355aec75aa3
Youssef Wagih <youssefwagih212@gmail.com>
2026-05-25 08:29:18 +0300

    .

--- Angular/Tasks/Day 4 & 5/Task/src/components/my-task-list/my-task-list.ts
+++ Angular/Tasks/Day 4 & 5/Task/src/components/my-task-list/my-task-list.ts
@@ -11,9 +11,30 @@ import { MyNotDoneTasks } from "../my-not-done-tasks/my-not-done-tasks";
   templateUrl: './my-task-list.html',
   styleUrl: './my-task-list.css',
 })
-export class MyTaskList implements OnChanges {
-  @Input() TasksFromApp: Task[] = [];
-  DisplayedTasks: Task[] = [];
+export class MyTaskList {
+  Tasks: Task[] = [
+    new Task('Task 1', 'Description for Task 1'),
+    new Task('Task 2', 'Description for Task 2'),
+    new Task('Task 3', 'Description for Task 3'),
+    new Task('Task 4', 'Description for Task 4'),
+    new Task('Task 5', 'Description for Task 5'),
+    new Task('Task 6', 'Description for Task 6'),
+    new Task('Task 7', 'Description for Task 7'),
+    new Task('Task 8', 'Description for Task 8'),
+    new Task('Task 9', 'Description for Task 9'),
+    new Task('Task 10', 'Description for Task 10'),
+    new Task('Task 11', 'Description for Task 11'),
+    new Task('Task 12', 'Description for Task 12'),
+    new Task('Task 13', 'Description for Task 13'),
+    new Task('Task 14', 'Description for Task 14'),
+    new Task('Task 15', 'Description for Task 15'),
+    new Task('Task 16', 'Description for Task 16'),
+    new Task('Task 17', 'Description for Task 17'),
+    new Task('Task 18', 'Description for Task 18'),
+    new Task('Task 19', 'Description for Task 19'),
+    new Task('Task 20', 'Description for Task 20'),
+  ];
+  DisplayedTasks: Task[] = [...this.Tasks];
   TabChoice = TabChoice;
 
   choice: TabChoice = TabChoice.ALL;
@@ -25,20 +46,20 @@ export class MyTaskList implements OnChanges {
   applyFilter() {
     switch (this.choice) {
       case TabChoice.ALL:
-        this.DisplayedTasks = [...this.TasksFromApp];
+        this.DisplayedTasks = [...this.Tasks];
         break;
       case TabChoice.DONE:
-        this.DisplayedTasks = [...this.TasksFromApp.filter(task => task.isDone)];
+        this.DisplayedTasks = [...this.Tasks.filter(task => task.isDone)];
         break;
       case TabChoice.NOT_DONE:
-        this.DisplayedTasks = [...this.TasksFromApp.filter(task => !task.isDone)];
+        this.DisplayedTasks = [...this.Tasks.filter(task => !task.isDone)];
         break;
     }
   }
   ngOnChanges(changes: SimpleChanges) {
-    console.log("Tasks in List onChanges: ", this.TasksFromApp);
-    console.log("Tasks in List onChanges: ", changes['TasksFromApp']?.firstChange);
-    if (changes['TasksFromApp']) {
+    console.log("Tasks in List onChanges: ", this.Tasks);
+    console.log("Tasks in List onChanges: ", changes['Tasks']?.firstChange);
+    if (changes['Tasks']) {
       this.applyFilter();
     }
 
@@ -46,8 +67,29 @@ export class MyTaskList implements OnChanges {
 
   @Output() SendTaskActionObjToApp = new EventEmitter<TaskAction>();
   ReceiveTaskActionObjFromFilteredList(TaskActionObj: TaskAction) {
-    this.SendTaskActionObjToApp.emit(TaskActionObj);
-
+    // this.SendTaskActionObjToApp.emit(TaskActionObj);
+    switch (TaskActionObj.action) {
+      case 'delete':
+        this.Tasks = this.Tasks.filter(task => task.id !== TaskActionObj.taskId);
+        this.DisplayedTasks = this.DisplayedTasks.filter(task => task.id !== TaskActionObj.taskId);
+        break;
+      case 'done':
+        this.Tasks = this.Tasks.map(task =>
+          task.id === TaskActionObj.taskId ? { ...task, isDone: true } : task
+        );
+        this.DisplayedTasks = this.DisplayedTasks.map(task =>
+          task.id === TaskActionObj.taskId ? { ...task, isDone: true } : task
+        );
+        break;
+      case 'not_done':
+        this.Tasks = this.Tasks.map(task =>
+          task.id === TaskActionObj.taskId ? { ...task, isDone: false } : task
+        );
+        this.DisplayedTasks = this.DisplayedTasks.map(task =>
+          task.id === TaskActionObj.taskId ? { ...task, isDone: false } : task
+        );
+        break;
+    }
   }
 
 }
