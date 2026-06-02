d4740f5b4e954ab1f529128877ec611b25c580ea
Youssef Wagih <youssefwagih212@gmail.com>
2026-06-02 03:49:30 +0300

    make sign up page reactive form

--- Angular/Tasks/Day 4 & 5/Task/src/components/my-sign-up/my-sign-up.ts
+++ Angular/Tasks/Day 4 & 5/Task/src/components/my-sign-up/my-sign-up.ts
@@ -1,36 +1,42 @@
 import { Component, inject, Inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-sign-up',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './my-sign-up.html',
   styleUrl: './my-sign-up.css',
 })
 export class MySignUp {
-  username: string = '';
-  email: string = '';
-  password: string = '';
+  signUpForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  })
 
   router = inject(Router);
   onSignUp() {
-    if (localStorage.getItem('username') === this.username){
+    if (this.signUpForm.invalid) {
+      alert('Please fill in all fields correctly.');
+      return;
+    }
+    if (localStorage.getItem('username') === this.signUpForm.value.username) {
       alert('Username already exists. Please choose a different username.');
       return;
     }
-    if (localStorage.getItem('email') === this.email){
+    if (localStorage.getItem('email') === this.signUpForm.value.email) {
       alert('Email already exists. Please choose a different email.');
       return;
     }
 
-    if (this.username === '' || this.email === '' || this.password === '') {
+    if (this.signUpForm.value.username === '' || this.signUpForm.value.email === '' || this.signUpForm.value.password === '') {
       alert('Please fill in all fields.');
       return;
     }
-    localStorage.setItem('username', this.username);
-    localStorage.setItem('email', this.email);
-    localStorage.setItem('password', this.password);
+    localStorage.setItem('username', this.signUpForm.get('username')?.value || '');
+    localStorage.setItem('email', this.signUpForm.get('email')?.value || '');
+    localStorage.setItem('password', this.signUpForm.get('password')?.value || '');
     alert('Sign up successful! You can now log in with your credentials.');
     this.router.navigate(['/login']);
   }
