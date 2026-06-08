e86309b86471e643156d5d84b5080880075c4aab
Youssef Wagih <youssefwagih212@gmail.com>
2026-06-08 06:08:51 +0300

    add signalr project cv

--- SignalR/Tasks/Lab2/Lab2MVC/Lab2MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab2MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
